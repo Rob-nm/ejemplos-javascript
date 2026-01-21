5d411cda9627f41ee2d499db15cb5e81b5533207
Rob-nm <gunnmr@gmail.com>
2026-01-21 14:27:02 -0600

    Calculadora terminada

--- calculadora.js
+++ calculadora.js
@@ -1,20 +1,37 @@
 // alert("Hola mundo");
 
-function mostrar(){
-
+function mostrar(e){
     let numero1 = parseInt(document.getElementById("numero1").value);
     let numero2 = parseInt(document.getElementById("numero2").value);
+    let resultado = 0;
+    let operacion = e.target.id;
 
-    
+    if (operacion == "suma"){
+        resultado = numero1 + numero2;
+    }
+    else if (operacion == "resta"){
+        resultado = numero1 - numero2;
+    }
+    else if (operacion == "divide"){
+        resultado = numero1 / numero2;
+    }
+    else if(operacion == "multiplica"){
+        resultado = numero1 * numero2;
+    }
+    else {
+        alert("Funcionalidad en progeso")
 
-  
-    let resultado =0;
-    resultado = numero1 + numero2;
+    }
 
-    let objResultado = document.getElementById("resultado")
-    objResultado.innerHTML = resultado;
+
+
+    let result = document.getElementById("resultado")
+    result.innerHTML = resultado;
 
    
     
 }
+
+let butones = document.getElementById("btns");
+butones.addEventListener("click", mostrar);
 
